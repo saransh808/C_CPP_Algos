32cfb0e8450b4eb2c76c1b5e471a143f0136cc97
Saransh Agarwal <saransh808@gmail.com>
2017-05-06 23:13:43 +0530

    #

--- Algorithms/Graph_coloring/GraphColoring.c
+++ Algorithms/Graph_coloring/GraphColoring.c
@@ -1,37 +1,38 @@
 #include<stdio.h>
-int G[50][50], X[50];
-
+int G[50][50],x[50];  //G:adjacency matrix,x:colors
 void next_color(int k){
-    int i,j;
-    X[k]=1;
-    for(i=0;i<k;i++){
-        if(G[i][k]!=0 && X[k]==X[i])
-            X[k]= X[i]+1;
-                    }
-                }
+   int i,j;
+   x[k]=1;  //coloring vertex with color1
+   for(i=0;i<k;i++){ //checking all k-1 vertices-backtracking
+     if(G[i][k]!=0 && x[k]==x[i])  //if connected and has same color
+       x[k]=x[i]+1;  //assign higher color than x[i]
+   }
+}
 
 int main(){
-    int n,e,i,j,k,l;
-    printf("enter number of vertices:");
-    scanf("%d",&n);
-    printf("enter number of edges:");
-    scanf("%d",&e);
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++)
-            G[i][j]=0;
-    }
+  int n,e,i,j,k,l;
+  printf("Enter no. of vertices : ");
+  scanf("%d",&n);  //total vertices
+  printf("Enter no. of edges : ");
+  scanf("%d",&e);  //total edges
+  
+  for(i=0;i<n;i++)
+    for(j=0;j<n;j++)
+      G[i][j]=0;  //assign 0 to all index of adjacency matrix
+      
+  printf("Enter indexes where value is 1-->\n");
+  for(i=0;i<e;i++){
+    scanf("%d %d",&k,&l);
+    G[k][l]=1;
+    G[l][k]=1;
+  }
+  
+  for(i=0;i<n;i++)
+    next_color(i);  //coloring each vertex
 
-    printf("enter indexes where value is 1:\n");
-    for(i=0;i<e;i++){
-        scanf("%d%d",&k,&l);
-        G[k][l] = 1;
-        G[l][k] = 1;
-    }
+  printf("Colors of vertices -->\n");
+  for(i=0;i<n;i++)  //displaying color of each vertex
+    printf("Vertex[%d] : %d\n",i+1,x[i]);
 
-    for(i=0;i<n;i++)
-        next_color(i);
-    printf("colors of verticesL\n");
-    for(i=0;i<n;i++)
-        printf("Vertex[%d]:%d\n",i+1,X[i]);
-    return 0;
+  return 0;
 }
